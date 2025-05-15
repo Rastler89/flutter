82cede39610f103d7e1ab98e53aecdb15be67bad
Rastler <rastler89@gmail.com>
2025-05-15 19:06:56 +0200

    dialogs

--- widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
+++ widgets_app/lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -26,11 +27,54 @@ class SnackbarScreen extends StatelessWidget {
     );
   }
 
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Hola'),
+          content: const Text('Este es un dialogo de ejemplo'),
+          actions: [
+            TextButton(
+              onPressed: () => context.pop(),
+              child: const Text('Cerrar')
+            ),
+            FilledButton(onPressed: () {}, child: const Text('Aceptar')),
+          ],
+        );
+      }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Snackbars y dialogs'),
+      ),
+      body: Center (
+        child: Column (
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: () {
+                showAboutDialog(
+                  context: context,
+                  children: const [
+                    Text('lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+                    Text('Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'),
+                  ]
+                );
+              },
+              child: const Text('Licencias usadas')
+            ),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context),
+              child: const Text('Mostrar dialogo')
+            )
+          ],
+        )
       ),
       floatingActionButton: FloatingActionButton.extended(
         label: const Text('Mostrar snackbar'),
