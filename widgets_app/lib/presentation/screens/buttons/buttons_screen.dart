57432dc152bcc656590bce244f5d13b802356b05
Rastler <rastler89@gmail.com>
2025-05-20 19:19:16 +0200

    tutorial finalizado

--- widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
+++ widgets_app/lib/presentation/screens/buttons/buttons_screen.dart
@@ -35,7 +35,7 @@ class _ButtonsView extends StatelessWidget {
           spacing: 10,
           children: [
             ElevatedButton(onPressed: () {}, child: const Text('Elevated Button')),
-            const ElevatedButton(onPressed: null, child: const Text('Elevated Disabled')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
             ElevatedButton.icon(onPressed: () {}, icon: const Icon(Icons.access_alarm_rounded), label: const Text('Elevated Icon')),
             FilledButton(onPressed: () {}, child: const Text('Filled Button')),
             FilledButton.icon(onPressed: () {}, icon: const Icon(Icons.accessibility), label: const Text('Filled Icon')),
