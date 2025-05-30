e39279f0a6079147b20ab9acbb8667f04f422129
Rastler <rastler89@gmail.com>
2025-05-30 11:52:36 +0200

    preparando slide

---
+++ cinemapedia/lib/infraestructure/datasources/moviedb_datasource.dart
@@ -0,0 +1,35 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/moviedb_response.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDatasource{
+
+  final dio = Dio(BaseOptions(
+    baseUrl: 'https://api.themoviedb.org/3',
+    queryParameters: {
+      'api_key': Environment.movieDB,
+      'language': 'es-ES',
+    }
+  ));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async{
+
+    final response = await dio.get('/movie/now_playing', queryParameters: {'page': page});
+
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDbResponse.results
+    .where((moviedb) => moviedb.posterPath != 'no-poster')
+    .map(
+      (e) => MovieMapper.movieDbToEntity(e)
+    ).toList();
+
+    return movies;
+  }
+
+  
+}
---
+++ cinemapedia/lib/presentation/widgets/movies/movies_lideshow.dart
@@ -0,0 +1,39 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:flutter/material.dart';
+
+class MoviesLideshow extends StatelessWidget {
+
+  final List<Movie> movies;
+  
+  const MoviesLideshow({
+    super.key,
+    required this.movies,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 210,
+      width: double.infinity,
+      child: Swiper(
+        itemCount: movies.length,
+        itemBuilder: (context, index) => _Slide(movie:movies[index]),
+        
+      )
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  final Movie movie;
+
+  const _Slide({
+    required this.movie,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder();
+  }
+}
